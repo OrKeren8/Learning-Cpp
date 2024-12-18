7db18c5f11d04964bee079c02997fcc739c929f0
or keren <orkeren8@gmail.com>
2024-12-19 01:45:03 +0200

    initialize client server

---
+++ udp-client/Source.cpp
@@ -0,0 +1,64 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <iostream>
+using namespace std;
+#pragma comment(lib, "Ws2_32.lib")
+#include <winsock2.h> 
+#include <string.h>
+
+#define TIME_PORT	27015
+
+void main()
+{
+
+	WSAData wsaData;
+	if (NO_ERROR != WSAStartup(MAKEWORD(2, 2), &wsaData))
+	{
+		cout << "Time Client: Error at WSAStartup()\n";
+		return;
+	}
+
+	SOCKET connSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (INVALID_SOCKET == connSocket)
+	{
+		cout << "Time Client: Error at socket(): " << WSAGetLastError() << endl;
+		WSACleanup();
+		return;
+	}
+
+	sockaddr_in server;
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_port = htons(TIME_PORT);
+
+	int bytesSent = 0;
+	int bytesRecv = 0;
+	char sendBuff[255] = "A";
+	char recvBuff[255];
+
+	bytesSent = sendto(connSocket, sendBuff, (int)strlen(sendBuff), 0, (const sockaddr*)&server, sizeof(server));
+	if (SOCKET_ERROR == bytesSent)
+	{
+		cout << "Time Client: Error at sendto(): " << WSAGetLastError() << endl;
+		closesocket(connSocket);
+		WSACleanup();
+		return;
+	}
+	cout << "Time Client: Sent: " << bytesSent << "/" << strlen(sendBuff) << " bytes of \"" << sendBuff << "\" message.\n";
+
+	bytesRecv = recv(connSocket, recvBuff, 255, 0);
+	if (SOCKET_ERROR == bytesRecv)
+	{
+		cout << "Time Client: Error at recv(): " << WSAGetLastError() << endl;
+		closesocket(connSocket);
+		WSACleanup();
+		return;
+	}
+
+	recvBuff[bytesRecv] = '\0'; //add the null-terminating to make it a string
+	cout << "Time Client: Recieved: " << bytesRecv << " bytes of \"" << recvBuff << "\" message.\n";
+
+	cout << "Time Client: Closing Connection.\n";
+	closesocket(connSocket);
+
+	system("pause");
+}
---
+++ udp-server/Source.cpp
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+#pragma comment(lib, "Ws2_32.lib")
+#include <winsock2.h>
+#include <string.h>
+#include <time.h>
+
+#define TIME_PORT	27015
+
+void main()
+{
+	WSAData wsaData;
+
+	if (NO_ERROR != WSAStartup(MAKEWORD(2, 2), &wsaData))
+	{
+		cout << "Time Server: Error at WSAStartup()\n";
+		return;
+	}
+
+	SOCKET m_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+	if (INVALID_SOCKET == m_socket)
+	{
+		cout << "Time Server: Error at socket(): " << WSAGetLastError() << endl;
+		WSACleanup();
+		return;
+	}
+
+	sockaddr_in serverService;
+	serverService.sin_family = AF_INET;
+	serverService.sin_addr.s_addr = INADDR_ANY;	//inet_addr("127.0.0.1");
+	serverService.sin_port = htons(TIME_PORT);
+
+	if (SOCKET_ERROR == bind(m_socket, (SOCKADDR*)&serverService, sizeof(serverService)))
+	{
+		cout << "Time Server: Error at bind(): " << WSAGetLastError() << endl;
+		closesocket(m_socket);
+		WSACleanup();
+		return;
+	}
+
+	sockaddr client_addr;
+	int client_addr_len = sizeof(client_addr);
+	int bytesSent = 0;
+	int bytesRecv = 0;
+	char sendBuff[255];
+	char recvBuff[2];
+
+	cout << "Time Server: Wait for clients' requests.\n";
+
+	while (true)
+	{
+		bytesRecv = recvfrom(m_socket, recvBuff, 255, 0, &client_addr, &client_addr_len);
+		if (SOCKET_ERROR == bytesRecv)
+		{
+			cout << "Time Server: Error at recvfrom(): " << WSAGetLastError() << endl;
+			closesocket(m_socket);
+			WSACleanup();
+			return;
+		}
+
+		recvBuff[1] = '\0'; //add the null-terminating to make it a string
+		cout << "Time Server: Recieved: " << bytesRecv << " bytes of \"" << recvBuff << "\" message.\n";
+
+		time_t timer;
+		time(&timer);
+		strcpy(sendBuff, ctime(&timer));
+		sendBuff[strlen(sendBuff) - 1] = '\0'; //to remove the new-line from the created string
+		
+		bytesSent = sendto(m_socket, sendBuff, (int)strlen(sendBuff), 0, (const sockaddr*)&client_addr, client_addr_len);
+		if (SOCKET_ERROR == bytesSent)
+		{
+			cout << "Time Server: Error at sendto(): " << WSAGetLastError() << endl;
+			closesocket(m_socket);
+			WSACleanup();
+			return;
+		}
+
+		cout << "Time Server: Sent: " << bytesSent << "\\" << strlen(sendBuff) << " bytes of \"" << sendBuff << "\" message.\n";
+	}
+
+	cout << "Time Server: Closing Connection.\n";
+	closesocket(m_socket);
+	WSACleanup();
+}
